3520654d4d629fa53e9af386468bddb11d95da14
Crystyan Santos <cryssantos9@gmail.com>
2023-07-03 14:54:25 -0300

    AuthGuard
    AuthGuard e Arg Param decorator

---
+++ src/utils/jwt-auth.guard.ts
@@ -0,0 +1,27 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common'
+import { GqlExecutionContext } from '@nestjs/graphql'
+import { JwtService } from '@nestjs/jwt'
+import { Observable } from 'rxjs'
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private jwtService: JwtService) {}
+  canActivate(
+    context: ExecutionContext
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const ctx = GqlExecutionContext.create(context)
+    const { req } = ctx.getContext()
+    //Bearer Token = split(): [Bearer, token] --> get pos[1y]
+    if (req.headers && req.headers.authorization.split(' ')[1]) {
+      const accessToken = req.headers.authorization.split(' ')[1]
+      try {
+        const payload = this.jwtService.verify(accessToken)
+        if (payload.scope.indexOf('accessToken') >= 0) {
+          req.user = payload.id
+          return true
+        }
+      } catch (err) {}
+    }
+    return false
+  }
+}
